bdbe9f486f6a20f96c57e7b6f80ed1d1c563b9ac
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-01-24 19:21:40 -0300

    Update products.py

--- routes/products.py
+++ routes/products.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, jsonify
+from services.product_service import get_all_products
 
+products_bp = Blueprint("products", __name__)
+
+@products_bp.route("/products", methods=["GET"])
+def products():
+    return jsonify(get_all_products())
