f8117830f13d55f48294ed31c6e74f3b07e039db
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-01-24 19:20:36 -0300

    Implement product management functions

--- services/product_service.py
+++ services/product_service.py
@@ -1,1 +1,16 @@
+from utils.file_db import read_data, write_data
 
+def get_all_products():
+    return read_data()
+
+def add_product(product):
+    data = read_data()
+    product["id"] = max([p["id"] for p in data], default=0) + 1
+    data.append(product)
+    write_data(data)
+    return product
+
+def delete_product(pid):
+    data = read_data()
+    data = [p for p in data if p["id"] != pid]
+    write_data(data)
