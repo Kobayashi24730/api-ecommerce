40e01af538bcfa24dd12d6abaed73944152d0537
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-01-24 20:24:31 -0300

    Update product.py

--- models/product.py
+++ models/product.py
@@ -1,1 +1,10 @@
+from pydantic import BaseModel
+from typing import List, Optional
 
+class Product(BaseModel):
+    id: Optional[int]
+    name: str
+    price: float
+    category: str
+    images: List[str]
+    description: Optional[str] = None
